0401037778e81ad95a1cc951b30ce21e7ad0681e
CarEsteban <car23016@uvg.edu.gt>
2024-07-25 20:15:00 -0600

    resolucion del postfix para operarlo

--- Calculator/src/main/kotlin/OperarPostfix.kt
+++ Calculator/src/main/kotlin/OperarPostfix.kt
@@ -1,11 +1,36 @@
+import com.sun.org.apache.xalan.internal.lib.ExsltMath.power
+import java.util.*
+
 class OperarPostfix(private val operacion:String) {
     private val caracteres = operacion.trim().replace(" ","").toList()
+    private val stack = Stack<String>()
+    private var operandoA = 0
+    private var operandoB = 0
 
-    fun mostrar(){
-        for (i in caracteres) {
-            print(i)
+    fun mostrarResultado(): String{
+
+        //validacion si es un numero o no
+        for(char in caracteres){
+            if(char.digitToIntOrNull() != null){
+                stack.push(char.toString())
+            }else{
+                operandoA = stack.pop().toInt()
+                operandoB = stack.pop().toInt()
+
+                when (char){
+                    '+' -> stack.push(sumar(operandoA,operandoB))
+                    //'-' -> stack.push(restar(operandoA,operandoB))
+                    //'*' -> stack.push(multiplicar(operandoA,operandoB))
+                    //'/' -> stack.push(dividir(operandoA,operandoB))
+                    //'^' -> stack.push(potenca(operandoA,operandoB))
+
+                }
+            }
         }
+        return stack.peek()
+
     }
+
 
 
 }
