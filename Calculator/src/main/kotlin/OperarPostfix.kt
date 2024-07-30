be53f95cf114c2c7e038cd5f27c5d4a9d846adcc
Nicolás <con23197@uvg.edu.gt>
2024-07-29 19:32:18 -0600

    operacion multiplicar

--- Calculator/src/main/kotlin/OperarPostfix.kt
+++ Calculator/src/main/kotlin/OperarPostfix.kt
@@ -20,7 +20,7 @@ class OperarPostfix(private val operacion:String) {
                 when (char){
                     '+' -> stack.push(sumar(operandoA,operandoB).toString())
                     '-' -> stack.push(restar(operandoA,operandoB).toString())
-                    //'*' -> stack.push(multiplicar(operandoA,operandoB).toString())
+                    '*' -> stack.push(multiplicar(operandoA,operandoB).toString())
                     //'/' -> stack.push(dividir(operandoA,operandoB).toString())
                     //'^' -> stack.push(potenca(operandoA,operandoB).toString())
 
@@ -33,5 +33,6 @@ class OperarPostfix(private val operacion:String) {
 
     private fun sumar(a: Int, b: Int) : Int{return a+b}
     private fun restar(a: Int, b: Int) : Int{return b-a}
+    private fun multiplicar(a: Int, b: Int) : Int{return a*b}
 
 }
